cfdb7166581c1f900b58dd64dde3987ee7d83a24
Omer Yassur <omeryassur@Omers-MacBook-Air.local>
2024-11-27 17:43:00 +0200

    submit

--- Algebra.java
+++ Algebra.java
@@ -6,14 +6,14 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
+	    System.out.println(plus(-2,-4));   // -2 + -4
+	    System.out.println(minus(-3,-3));  // -4 - -3
    		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
+ 		System.out.println(times(-3,-4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
+   		System.out.println(pow(-5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(-14,2));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
@@ -25,9 +25,13 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		int result = x1;
+		int result = x1;		
 		for (int i=0; i<x2; i++) {
 			result++;
+		}
+		
+		for (int i=0; i>x2; i--) {
+			result--;
 		}
 		return result;
 	}
@@ -38,6 +42,11 @@ public class Algebra {
 		for (int i=0; i<x2; i++) {
 			result--;
 		}
+
+		for (int i=0; i>x2; i--) {
+			result++;
+		}
+
 		return result;
 	}
 
@@ -46,6 +55,9 @@ public class Algebra {
 		int result = 0;
 		for (int i = 0; i<x2; i++) {
 			result = plus(result,x1); // result plus x1;
+		}
+		for (int i = 0; i>x2; i--) {
+			result = minus(result,x1); // result plus x1;
 		}
 		return result;
 	}
@@ -61,13 +73,20 @@ public class Algebra {
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int result = 0;
+		boolean isNegative = (x1 < 0) != (x2 < 0);
+		x1 = Math.abs(x1);
+		x2 = Math.abs(x2);
+		int result =0;
 		while (x1>=x2) {
 			x1 = minus(x1, x2);
 			result++;
 		}
+		if (isNegative) {
+			result=times(result, -1);
+		}
 		return result;
 	}
+	
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
@@ -83,6 +102,10 @@ public class Algebra {
 	while (times(i,i)<x) {
 		i++;
 	}
+	if (times(i,i)>x) {
+		i--;
+	}
+	
 	return i;
 	}	  	  
 }
